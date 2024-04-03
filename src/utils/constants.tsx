e0d457062335997a737db1754f8c47f8c4657374
Harshit Siwach <37680927+harshitsiwach@users.noreply.github.com>
2024-04-04 05:05:07 +0530

    updated to eth mainnet

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,8 +1,9 @@
 // Define and export your constants here
 export const CONTRACT_ADDRESSES = {
-    nftContract: "0x5E85Dde64773920fAff72dB023fCFa7c95fC19BF",
+    nftContract: "0x7FCBb823ff16110E5A14C3c897dC0af334423E4f",
     // You can add other contract addresses or related constants here
     // Mistle Toadz CA: "0x7FCBb823ff16110E5A14C3c897dC0af334423E4f"
+    // Base sepolia testing CA: "0x5E85Dde64773920fAff72dB023fCFa7c95fC19BF"
   };
   
  
