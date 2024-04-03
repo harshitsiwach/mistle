ef77cfc93fbe856b4cee1f23763c04e025c1d682
Harshit Siwach <37680927+harshitsiwach@users.noreply.github.com>
2024-04-04 04:00:16 +0530

    changed to base

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,7 +1,8 @@
 // Define and export your constants here
 export const CONTRACT_ADDRESSES = {
-    nftContract: " 0x7FCBb823ff16110E5A14C3c897dC0af334423E4f",
+    nftContract: "0x5E85Dde64773920fAff72dB023fCFa7c95fC19BF",
     // You can add other contract addresses or related constants here
+    // Mistle Toadz CA: "0x7FCBb823ff16110E5A14C3c897dC0af334423E4f"
   };
   
  
